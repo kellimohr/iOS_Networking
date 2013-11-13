82becc9dbea18be783066830a25d9c858d6aad31
kellimohr <kellimohr@gmail.com>
2013-11-13 13:43:03 -0800

    Initial Commit

---
+++ Networking/KMViewController.h
@@ -0,0 +1,13 @@
+//
+//  KMViewController.h
+//  Networking
+//
+//  Created by Kelli Mohr on 11/13/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMViewController : UIViewController
+
+@end
---
+++ NetworkingTests/NetworkingTests.m
@@ -0,0 +1,34 @@
+//
+//  NetworkingTests.m
+//  NetworkingTests
+//
+//  Created by Kelli Mohr on 11/13/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NetworkingTests : XCTestCase
+
+@end
+
+@implementation NetworkingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
